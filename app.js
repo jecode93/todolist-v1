2f569af888829bbaf46496b3e11e8c3691e2ccab
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-12-17 17:50:53 -0500

    Creating my first EJS templates.

--- app.js
+++ app.js
@@ -2,17 +2,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
 
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Thusday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thuesday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("yay it's the weekend.");
-    } else {
-        res.send("Boo! I have to work.");
+        default:
+            console.log("Error : The current day error " + currentDay);
+            break;
     }
 
+    res.render("list", { kindOfDay: day });
 });
 
 
