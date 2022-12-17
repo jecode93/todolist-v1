9a5daac29ab34bdb6b1e9ec7485e8c9d7612f628
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-12-17 16:28:01 -0500

    First commit of the todo list app.

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello");
+});
+
+
+app.listen(3000, function (req, res) {
+    console.log("Server started on port 3000");
+});
