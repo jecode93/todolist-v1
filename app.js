f11ac58400503e24001dd54a8a324c556fe10fc6
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-12-26 19:21:10 -0500

    Create list using ejs, date, and for loop for the app.

--- app.js
+++ app.js
@@ -2,44 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = [];
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
 
     var today = new Date();
     var currentDay = today.getDay();
-    var day = "";
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Thusday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thuesday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
 
-        default:
-            console.log("Error : The current day error " + currentDay);
-            break;
-    }
+    var options = {
+        weekday: "long",
+        // year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
 
-    res.render("list", { kindOfDay: day });
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", { kindOfDay: day, newListItems: items });
 });
 
+
+app.post("/", function (req, res) {
+    var item = req.body.enterInfo;
+    items.push(item);
+
+    res.redirect("/");
+})
 
 app.listen(3000, function (req, res) {
     console.log("Server started on port 3000");
