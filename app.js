3a9c5f93ec59f0d480c0b767996999d8e2af36e5
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-12-27 07:23:12 -0500

    Add new pages to the app and style the app with css, add for loop to show the items.

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = [];
+let workItems = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
@@ -21,16 +23,30 @@ app.get("/", function (req, res) {
     let day = today.toLocaleDateString("en-US", options);
 
 
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
 
 app.post("/", function (req, res) {
     let item = req.body.enterInfo;
-    items.push(item);
 
-    res.redirect("/");
-})
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+// app.post("/work", function (req, res) {
+//     let item = req.body.enterInfo;
+//     workItems.push(item);
+// })
 
 app.listen(3000, function (req, res) {
     console.log("Server started on port 3000");
